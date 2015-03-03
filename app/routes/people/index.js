22c9cb878903473ad33dca6a5e7ec6e96dbd54f0
Walter McGinnis <wm@waltermcginnis.com>
2015-03-03 20:49:45 +1300

    Switch to ic-ajax to return promise, refactor test
    test is still WIP

--- app/routes/people/index.js
+++ app/routes/people/index.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 import config from '../../config/environment';
+import ajax from 'ic-ajax';
 
 export default Ember.Route.extend({
   model: function() {
     var address = this.controllerFor('application').get('address');
     var url = 'http://' + config.APP.askThemHost + '/locator.json?q=' + address;
-    return Ember.$.getJSON(url);
+    return ajax(url);
   }
 });
--- tests/unit/routes/people/index-test.js
+++ tests/unit/routes/people/index-test.js
@@ -1,5 +1,3 @@
-// import Ember from 'ember';
-
 import {
   moduleFor,
   test
@@ -9,15 +7,15 @@ moduleFor('route:people/index', {
   needs: ['controller:application']
 });
 
-// TODO: pending, need advice on on how to test this!!!
-//
 // test('model returns array of people', function(assert) {
 //   var route = this.subject(),
 //       applicationController = route.controllerFor('application');
 
 //   applicationController.set('address', '05602');
 
-//   Ember.run(function() {
-//     assert.equal(route.send('model'), {});
-//   });
+//   route.get('model').then(
+//     function(result) {
+//       assert.equal(result, {});
+//     }
+//   );
 // });
