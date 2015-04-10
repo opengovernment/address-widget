f4c104c03c205d88a0ece0aa23764e255eaeb0b7
Walter McGinnis <wm@waltermcginnis.com>
2015-04-10 15:28:30 +1200

    Update ember-cli, ember, & other dependencies
    * also make adjustments to code necessitated by updates

--- app/router.js
+++ app/router.js
@@ -5,14 +5,10 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
   this.resource('people', function() {});
-
   this.route('questions', function() {
     this.route('new');
   });
-
   this.route('thanks');
 });
-
-export default Router;
